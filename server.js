9e47aa7dfd9c86b935ed8f8cec43c6700f3b7340
Ochouno2364 <ochouno2364@gmail.com>
2025-01-16 21:22:07 -0500

    done1

--- server.js
+++ server.js
@@ -55,6 +55,15 @@ res.send(`So, you want the ${collectibles[index].name} For ${collectibles[index]
 
 //Exercise 4
 
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
 
 app.get('/shoes', (req,res)=> {
